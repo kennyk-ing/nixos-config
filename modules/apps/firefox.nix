8209b81ef46ae8ba788e4f7672016776ee3b5db4
Kenny King <16931002+kennyk-ing@users.noreply.github.com>
2026-07-04 17:59:09 -0700

    firefox: config changes to reduce lag/stutter

--- modules/apps/firefox.nix
+++ modules/apps/firefox.nix
@@ -1,4 +1,5 @@
 { lib, config, ... }:
+
 let
   cfg = config.mySystem.apps.firefox;
 in
@@ -8,7 +9,20 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    programs.firefox.enable = true;
+    programs.firefox = {
+      enable = true;
+
+      preferences = {
+        # --- Hardware Acceleration (VA-API) ---
+        "media.ffmpeg.vaapi.enabled" = true;
+        "gfx.webrender.all" = true;
+
+        # --- UI Performance Fixes ---
+        # Disables accessibility services to fix extreme typing/UI lag on Wayland
+        "accessibility.force_disabled" = 1;
+
+        "general.smoothScroll" = true;
+      };
+    };
   };
 }
-
