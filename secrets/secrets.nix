213415bc476bf549c9bf30d4c0d1edaadf2baa2f
Kenny King <16931002+kennyk-ing@users.noreply.github.com>
2026-07-06 10:21:39 -0700

    secrets: add keys

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,9 +1,10 @@
 let
-  kenny = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE0GBm6VaFt7sMzUy5gCiS9umQmuPxtfskzO+GkXEvnP kenny@kirby";
+  kenny_kirby = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE0GBm6VaFt7sMzUy5gCiS9umQmuPxtfskzO+GkXEvnP kenny@kirby";
+  kenny_woo = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEPn3sIhk+pS0a5LSppmHbLY98Wktsi6QnH0V4XGf/5m kenny@woo";
   kirby = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDgY9NgbiN6lb7YDx135NPBTwWYO7r8/oX3ALweQwQQ2 root@kirby";
   woo = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBKRzPZ5rAtMrEL3Vo0Jdfb58GyWktgZ93MP6gu0wwo1 root@woo";
 in
 {
-  "wifi.age".publicKeys = [ kenny kirby woo ];
-  "email_personal.age".publicKeys = [ kenny kirby woo ];
+  "wifi.age".publicKeys = [ kenny_kirby kenny_woo kirby woo ];
+  "email_personal.age".publicKeys = [ kenny_kirby kenny_woo kirby woo ];
 }
