b09138a98b5f82ff832901617121cec2d45bc288
Kenny King <16931002+kennyk-ing@users.noreply.github.com>
2026-09-05 05:15:04 -0700

    remove duplicate user module imports

--- modules/default.nix
+++ modules/default.nix
@@ -2,32 +2,34 @@
 
 {
   imports = [
-    ./apps/emacs.nix
     ./apps/browsers.nix
+    ./apps/emacs.nix
     ./apps/gaming.nix
     ./apps/nixvim
     ./apps/office.nix
     ./apps/wezterm.nix
+
     ./desktop/gdm.nix
     ./desktop/sddm.nix
     ./desktop/niri
     ./desktop/plasma.nix
+
     ./hardware/intel-graphics.nix
     ./hardware/laptop.nix
     ./hardware/tplink-ethernet2usb.nix
     ./hardware/wifi.nix
+
     ./profiles/development.nix
+
     ./services/openssh.nix
+    ./services/plex.nix
     ./services/podman.nix
     ./services/syncthing.nix
     ./services/tailscale.nix
-    ./services/plex.nix
+
     ./system/core.nix
-    ./system/workstation.nix
     ./system/graphics.nix
+    ./system/workstation.nix
     ./system/zram.nix
-    ../users/kenny
-    ../users/kenny/workstation.nix
-    ../users/karen
   ];
 }
