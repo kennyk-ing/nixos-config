7e3e6bdf40b07b92bbb5bd8eddf8ead82204f8ec
Kenny King <16931002+kennyk-ing@users.noreply.github.com>
2026-09-15 18:29:50 -0700

    kirby: move server to vlan 100

--- hosts/kirby/networking.nix
+++ hosts/kirby/networking.nix
@@ -1,10 +1,54 @@
 { ... }:
 
 {
-  systemd.network.networks."10-lan" = {
-    # Using MAC instead of device name because it's a USB device that may move
-    matchConfig.MACAddress = "9c:69:d3:81:48:ea";
+  systemd.network = {
+    netdevs = {
+      "20-vlan100" = {
+        netdevConfig = {
+          Kind = "vlan";
+          Name = "vlan100";
+        };
+        vlanConfig.Id = 100;
+      };
 
-    networkConfig.DHCP = "ipv4";
+      "20-vlan110" = {
+        netdevConfig = {
+          Kind = "vlan";
+          Name = "vlan110";
+        };
+        vlanConfig.Id = 110;
+      };
+    };
+
+    networks = {
+      "10-parent" = {
+        matchConfig.PermanentMACAddress = "9c:69:d3:81:48:ea";
+
+        vlan = [
+          "vlan100"
+          "vlan110"
+        ];
+
+        networkConfig.LinkLocalAddressing = "no";
+        linkConfig.RequiredForOnline = "no";
+      };
+
+      # Kirby's eventual management/server network.
+      "30-vlan100" = {
+        matchConfig.Name = "vlan100";
+
+        networkConfig.DHCP = "ipv4";
+        linkConfig.RequiredForOnline = "no";
+      };
+
+      # Carried to Kirby for isolated workloads. The Kirby host itself
+      # must not acquire an address or route on VLAN 110.
+      "30-vlan110" = {
+        matchConfig.Name = "vlan110";
+
+        networkConfig.LinkLocalAddressing = "no";
+        linkConfig.RequiredForOnline = "no";
+      };
+    };
   };
 }
