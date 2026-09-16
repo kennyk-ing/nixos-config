d30f5ca7f5aaf8a4e3689a09631cb1d6ac6adb5a
Kenny King <16931002+kennyk-ing@users.noreply.github.com>
2026-09-15 18:48:04 -0700

    kenny:zsh: reconfigure powerlevel10k

--- users/kenny/apps/zsh.nix
+++ users/kenny/apps/zsh.nix
@@ -1,17 +1,24 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 {
+  home.file.".p10k.zsh".source = ../files/p10k.zsh;
+
   programs.zsh = {
     enable = true;
+
     shellAliases = {
       ll = "ls -l";
+      la = "ls -a";
       nixsw = "nixos-rebuild switch --flake ~/configs/nixos --sudo";
     };
+
     dotDir = "${config.xdg.configHome}/zsh";
+
     enableCompletion = true;
     syntaxHighlighting.enable = true;
     autosuggestion.enable = true;
     autocd = true;
+
     history = {
       append = true;
       ignoreAllDups = true;
@@ -19,7 +26,9 @@
       size = 50000;
     };
     historySubstringSearch.enable = true;
+
     defaultKeymap = "viins"; # vi mode
+
     antidote = {
       enable = true;
       useFriendlyNames = true;
@@ -28,11 +37,22 @@
         "ohmyzsh/ohmyzsh path:plugins/sudo"
       ];
     };
+
+    plugins = [
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+    ];
+
     initContent = ''
       # create a directory and cd into it
       mdcd() {
         mkdir -p "$@" && cd "$@"
       }
+
+      [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
     '';
   };
 }
--- users/kenny/workstation.nix
+++ users/kenny/workstation.nix
@@ -40,22 +40,7 @@ in
       # Make sure home manager can install fonts to the system catalog
       fonts.fontconfig.enable = true;
 
-      home.file.".p10k.zsh".source = ./files/p10k.zsh;
-
       programs = {
-        zsh = {
-          plugins = [
-            {
-              name = "powerlevel10k";
-              src = pkgs.zsh-powerlevel10k;
-              file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-            }
-          ];
-          initContent = ''
-            [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-          '';
-        };
-
         obsidian = {
           enable = true;
         };
